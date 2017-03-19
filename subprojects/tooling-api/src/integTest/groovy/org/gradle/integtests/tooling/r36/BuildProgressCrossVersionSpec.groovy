2a9c7c3a42e3acfab6b30cfbc84f58cd1fc34180
Paul Merlin <paul@gradle.com>
2017-03-19 18:36:15 +0100

    Worker lease and build operation for the whole build execution
    DefaultGradleLauncher now holds a worker lease around the whole build
    execution. This will allow for worker lease management at configuration
    time.
    
    It now also emits a ‘Run build’ build operation around the whole build
    execution. Previously it was only emitted when executing a build through
    TAPI.

--- subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r36/BuildProgressCrossVersionSpec.groovy
+++ subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r36/BuildProgressCrossVersionSpec.groovy
@@ -227,7 +227,6 @@ class BuildProgressCrossVersionSpec extends ToolingApiSpecification {
                 connection.newBuild()
                     .withArguments('--init-script', initScript1.toString(), '--init-script', initScript2.toString())
                     .addProgressListener(events)
-                    .run()
         }
 
         then:
@@ -454,5 +453,4 @@ class BuildProgressCrossVersionSpec extends ToolingApiSpecification {
     MavenFileRepository getMavenRepo(String name = "repo") {
         return new MavenFileRepository(file(name))
     }
-
 }
